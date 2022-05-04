f97fc2980c87c0b2cd551445e1b6a691916656e5
ZilverBlade <90721817+ZilverBlade@users.noreply.github.com>
2022-05-04 18:30:52 +0100

    changed how project references headers from glfw. No longer needed to install glfw

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,4 +1,4 @@
-D:\GameEngines\Vulkan-1.3.211.0\Bin\glslc.exe shaders/ez_shader.vert -o shaders/ez_shader.vert.spv
-D:\GameEngines\Vulkan-1.3.211.0\Bin\glslc.exe shaders/ez_shader.frag -o shaders/ez_shader.frag.spv
+glslc.exe shaders/ez_shader.vert -o shaders/ez_shader.vert.spv
+glslc.exe shaders/ez_shader.frag -o shaders/ez_shader.frag.spv
 
 pause
--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
 
     Py_Initialize();
 
-    PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+    PyRun_SimpleString("print('Hello World from Embedded Python!')");
 
     Py_Finalize();
 
