a7f42005689b9959fb30d9bfc024c51dfb2790eb
ZilverBlade <90721817+ZilverBlade@users.noreply.github.com>
2022-05-10 20:24:18 +0100

    added index buffers to significantly reduce gpu memory

--- shard_model.cpp
+++ shard_model.cpp
@@ -2,31 +2,87 @@
 #include <cassert>
 namespace shard {
 
-	ShardModel::ShardModel(ShardDevice& device, const std::vector<Vertex>& vertices) : shardDevice{device} {
-		createVertexBuffers(vertices);
+	ShardModel::ShardModel(ShardDevice& device, const ShardModel::Builder &builder) : shardDevice{device} {
+		createVertexBuffers(builder.vertices);
+		createIndexBuffers(builder.indices);
 	}
 	ShardModel::~ShardModel() {
 		vkDestroyBuffer(shardDevice.device(), vertexBuffer, nullptr);
 		vkFreeMemory(shardDevice.device(), vertexBufferMemory, nullptr);
+
+		if (hasIndexBuffer) {
+			vkDestroyBuffer(shardDevice.device(), indexBuffer, nullptr);
+			vkFreeMemory(shardDevice.device(), indexBufferMemory, nullptr);
+		}
 	}
 
 	void ShardModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
 		vertexCount = (uint32_t)(vertices.size());
-
 		assert(vertexCount >= 3 && "Vertex count must be at least 3");
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
 		shardDevice.createBuffer(
 			bufferSize,
-			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory
+		);
+
+		void* data;
+		vkMapMemory(shardDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(shardDevice.device(), stagingBufferMemory);
+
+		shardDevice.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 			vertexBuffer,
 			vertexBufferMemory
 		);
 
+		shardDevice.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+		vkDestroyBuffer(shardDevice.device(), stagingBuffer, nullptr);
+		vkFreeMemory(shardDevice.device(), stagingBufferMemory, nullptr);
+	}
+
+	void ShardModel::createIndexBuffers(const std::vector<uint32_t> &indices) {
+		indexCount = (uint32_t)(indices.size());
+		hasIndexBuffer = indexCount > 0;
+
+		if (!hasIndexBuffer) { return; }
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		shardDevice.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory
+		);
+
 		void* data;
-		vkMapMemory(shardDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(shardDevice.device(), vertexBufferMemory);
+		vkMapMemory(shardDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(shardDevice.device(), stagingBufferMemory);
+
+		shardDevice.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			indexBuffer,
+			indexBufferMemory
+		);
+
+		shardDevice.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		vkDestroyBuffer(shardDevice.device(), stagingBuffer, nullptr);
+		vkFreeMemory(shardDevice.device(), stagingBufferMemory, nullptr);
 	}
 
 
@@ -34,8 +90,17 @@ namespace shard {
 		VkBuffer buffers[] = { vertexBuffer };
 		VkDeviceSize offsets[] = { 0 };
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+		if (hasIndexBuffer) {
+			vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		}
 	}
 	void ShardModel::draw(VkCommandBuffer commandBuffer) {
+		if (hasIndexBuffer) {
+			vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
+		} else {
+			vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+		}
 		vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
 	}
 
--- shard_model.hpp
+++ shard_model.hpp
@@ -20,7 +20,13 @@ namespace shard {
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 		};
-		ShardModel(ShardDevice &device, const std::vector<Vertex> &vertices);
+
+		struct Builder {
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		ShardModel(ShardDevice &device, const ShardModel::Builder &builder);
 		~ShardModel();
 
 		ShardModel(const ShardModel&) = delete;
@@ -28,13 +34,20 @@ namespace shard {
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
-
 		
 	private:
 		void createVertexBuffers(const std::vector<Vertex> &vertices);
+		void createIndexBuffers(const std::vector<uint32_t> &indices);
+
 		ShardDevice& shardDevice;
+
 		VkBuffer vertexBuffer;
 		VkDeviceMemory vertexBufferMemory;
 		uint32_t vertexCount;
+
+		bool hasIndexBuffer = false;
+		VkBuffer indexBuffer;
+		VkDeviceMemory indexBufferMemory;
+		uint32_t indexCount;
 	};
 }
